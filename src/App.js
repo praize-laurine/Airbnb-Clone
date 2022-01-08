c9f7ae932cdf487b7102aefa0eb5efe2b06f784c
praize-laurine <preyzelaurine@gmail.com>
2022-01-08 04:20:21 +0300

    finished the build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Footer from './Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from './SearchPage';
 
 function App() {
   return (
 
     //BEM
     <div className="app">
+      <Router>
+
       <Header/>
-      <Home/>
+
+      <Switch >
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      <Footer />
+
+      </Router>
       
 
 
--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,14 @@ import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <div className='header'>
+            <Link to='/'>
             <img className="header__icon" src="https://blogravenridgecottages.files.wordpress.com/2021/04/airbnb-logo.jpeg" alt="" />
+            </Link>
 
             <div className='header__center'>
                 <input type="text" />
